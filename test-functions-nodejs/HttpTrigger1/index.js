de7d15b3dd6e2e81776dbee93e69b251352da880
hossam-nasr <hossam.nasr99@gmail.com>
2022-02-10 16:51:02 -0800

    modify some funcs

--- test-functions-nodejs/HttpTrigger1/index.js
+++ test-functions-nodejs/HttpTrigger1/index.js
@@ -7,8 +7,9 @@ module.exports = async function (context, req) {
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.\n" +
         "The value of user is \n" + JSON.stringify(context.req.user);
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    // context.res = {
+    //     status: 400, /* Defaults to 200 */
+    //     body: responseMessage
+    // };
+    context.res.status(400).json({"hello": "world"});
 }
