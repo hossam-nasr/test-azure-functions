6fee2e42135fc453c07c85a6f305879ba81ef502
hossam-nasr <hossam.nasr99@gmail.com>
2022-08-30 16:56:38 -0700

    Add durable entity

---
+++ durable-js/DurableFunctionsEntityHttpStart/index.ts
@@ -0,0 +1,20 @@
+﻿import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import * as df from "durable-functions"
+
+const httpStart: AzureFunction = async function (context: Context, req: HttpRequest) {
+    throw new Error("test")
+    // const client = df.getClient(context);
+    // const id: string = req.params.id;
+    // const entityId = new df.EntityId("Counter", null);
+
+    // if (req.method === "POST") {
+    //     // increment value
+    //     await client.signalEntity(entityId, "add", 1);
+    // } else {
+    //     // reads current state of entity
+    //     const stateResponse = await client.readEntityState<number>(entityId);
+    //     return { body: stateResponse.entityState };
+    // }
+};
+
+export default httpStart;
---
+++ durable-js/OrchestratorSignalEntity/index.ts
@@ -0,0 +1,25 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an HTTP starter function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable activity function (default name is "Hello")
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your 
+ *    function app in Kudu
+ */
+
+import * as df from "durable-functions"
+
+const orchestrator = df.orchestrator(function* (context) {
+    const id:string = context.df.getInput()
+    // const entityId = new df.EntityId("Counter", null);
+    throw new Error("test")
+
+    //yield context.df.signalEntity(entityId, "add", 1)
+    //const outputs = ['signalled'];
+    //outputs.push(yield context.df.callActivity("Hello", "Seattle"))
+    return 'Success!'
+});
+
+export default orchestrator;
